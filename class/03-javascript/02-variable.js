acb85f3870f66e50f3b283fdc3bedc40fc2b8e39
mineki <mineki94@naver.com>
2024-08-14 16:08:38 +0900

    # 자바스크립트 - 변수, 상수
    자바스크립트 연결방법, 변수,상수 생서및 선언 방법등

---
+++ class/03-javascript/02-variable.js
@@ -0,0 +1,13 @@
+console.log("콘솔 로그 찍어보기")
+let name
+console.log(name)
+name = "홍길동"
+console.log(name)
+name = "김삿갓"
+console.log(name)
+// let name = "재설정못함"
+
+const name2 = "홍길동"
+console.log(name2)
+// const name2 = "const는 재설정안됨"
+// name2 = "재할당도 안됨"
