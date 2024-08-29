94ce3a598e073f50bdeb1c0fcbaa17ad1b6943b1
mineki <mineki94@naver.com>
2024-08-29 19:59:31 +0900

    # Todo-List v3
    전체 삭제버튼을 추가, 로컬스토리지에 저장하기 위해서 리스트의 값과 완료 여부를 가져오는 내용 추가 ( 아직 스토리지에 추가 기능은 없음 )

--- 03_js_basic/Todo-list/index.js
+++ 03_js_basic/Todo-list/index.js
@@ -1,8 +1,10 @@
 // 완료 여부 체크를 위한 EventListener 추가
+
+const todoInput = document.querySelector('#todo-input');
+const todoList= document.querySelector('#todo-list');
+
 const newTag = function () {
-        const todoInput = document.querySelector('#todo-input');
     if (window.event.keyCode === 13 && todoInput.value){
-        const todoList= document.querySelector('#todo-list');
         const newLi = document.createElement('li')
         const newSpan = document.createElement('span')
         const newBtn = document.createElement('button')
@@ -11,11 +13,40 @@ const newTag = function () {
             newLi.classList.toggle('complete')
         })
 
+        newLi.addEventListener('dblclick',() => {
+            newLi.remove()
+        })
+
         newSpan.textContent = todoInput.value;
         newLi.appendChild(newBtn);
         newLi.appendChild(newSpan);
         todoList.appendChild(newLi);
-        console.log(newLi)
         todoInput.value ='';
+        saveItemsFn()
     }
+}
+
+// 전체 삭제 버튼 기능
+const deleteAll = function () {
+    const liList = document.querySelectorAll('li');
+    for(let i = 0 ; i < liList.length; i ++ ){
+        liList[i].remove();
+    }
+}
+
+// 로컬 스토리지에 저장
+const saveItemsFn = function () {
+    const saveItems = [];
+    // 데이터를 가져오는 방법
+    // console.log(todoList.children[0].querySelector('span').textContent);
+    // 위의 방식을 사용할것.
+
+    for(let i = 0 ; i < todoList.children.length; i++ ){
+        const todoObj = {
+            contents : todoList.children[i].querySelector('span').textContent,
+            complete : todoList.children[i].classList.contains('complete')  
+        };
+        saveItems.push(todoObj);
+    }
+    console.log(saveItems);
 }
