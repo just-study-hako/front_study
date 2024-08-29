f6254cab9d5e7dca449080904b95285e11f5db09
mineki <mineki94@naver.com>
2024-08-29 19:24:02 +0900

    # Todo-List v2
    addEventListener를 사용해서 완료된 할일에 취소선 생기도록 수정, 버튼을 만들고 버튼의 외형 css 적용

--- 03_js_basic/Todo-list/index.js
+++ 03_js_basic/Todo-list/index.js
@@ -1,28 +1,21 @@
-const checkInput = function () {
-    // 인풋 박스에 입력된 데이터 가져오려면
-    const inputValue = document.querySelector('#todo-input').value;
-    console.log(inputValue)
-}
-
-// 특정 키를 인식하여 함수를 동작시키기
-const keyCodeCheck = function () {
-    if (window.event.keyCode === 13){
-        const inputValue = document.querySelector('#todo-input').value;
-        console.log(inputValue)
-    }
-}
-
-// 새로운 li 태그를 만들기
+// 완료 여부 체크를 위한 EventListener 추가
 const newTag = function () {
         const todoInput = document.querySelector('#todo-input');
     if (window.event.keyCode === 13 && todoInput.value){
         const todoList= document.querySelector('#todo-list');
         const newLi = document.createElement('li')
         const newSpan = document.createElement('span')
+        const newBtn = document.createElement('button')
+
+        newBtn.addEventListener('click',() => {
+            newLi.classList.toggle('complete')
+        })
 
         newSpan.textContent = todoInput.value;
+        newLi.appendChild(newBtn);
         newLi.appendChild(newSpan);
-        todoList.appendChild(newLi)
+        todoList.appendChild(newLi);
+        console.log(newLi)
         todoInput.value ='';
     }
 }
