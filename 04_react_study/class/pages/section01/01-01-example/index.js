d26bece77cd21237db6bc6c46a97002c18b21570
mineki <mineki94@naver.com>
2024-09-03 17:39:15 +0900

    # example
    리액트로 기본 화면 만들어 보기

---
+++ 04_react_study/class/pages/section01/01-01-example/index.js
@@ -0,0 +1,14 @@
+export default function 나만의페이지() {
+    const aaa = 3 // => 위쪽은 자바 스크립트 공간
+    
+
+    // 아래는 html 공간
+    return (
+        <div>
+            <div>철수</div>
+            <button>클릭하세요</button>
+        </div>
+    )
+    
+    
+}
