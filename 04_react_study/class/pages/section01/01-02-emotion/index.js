1dabce80865cd54e66ee0ec2e969ac455a56a82d
mineki <mineki94@naver.com>
2024-09-03 17:39:43 +0900

    # emotion
    리액트로 css 만들어서 적용 시켜보기

---
+++ 04_react_study/class/pages/section01/01-02-emotion/index.js
@@ -0,0 +1,17 @@
+import {MyEmail,MyInput} from '../../../styles/emotion'
+
+export default function EmotionPage() {
+
+    // 자바스크립트 공간
+
+    return(
+        <div>
+            <MyEmail>이메일 : </MyEmail>
+            <MyInput type="text"/>
+            <button>로그인</button>
+            <img src="/next.svg"></img>
+        </div>
+    )
+
+
+}
---
+++ 04_react_study/class/styles/emotion.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+
+export const MyEmail = styled.span`
+    color: blue;
+    font-size : 25px;
+`
+
+export const MyInput = styled.input`
+    border-color:red;
+    border-radius:5px;
+`
