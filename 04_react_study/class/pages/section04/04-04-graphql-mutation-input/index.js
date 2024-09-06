0204c4d52561b5aa2bee0b3a3469ff84bceebfac
mineki <mineki94@naver.com>
2024-09-06 12:57:56 +0900

    # 값을 직접 넣기
    값을 input으로 받아서 입력하는 방식

---
+++ 04_react_study/class/pages/section04/04-04-graphql-mutation-input/index.js
@@ -0,0 +1,58 @@
+import { useMutation ,gql} from "@apollo/client"
+import { useState } from "react"
+
+const 나의그래프큐엘셋팅 = gql`
+    mutation createBoard($writer: String, $title: String, $contents: String){
+
+
+        createBoard(writer: $writer, title: $title, contents: $contents){
+            __id
+            number
+            message
+        }
+    }
+`
+
+
+export default function GraphqlMutationPage (){
+    const [writer,setWriter] = useState()
+    const [title,setTitle] = useState()
+    const [contents,setContents] = useState()
+
+    const [나의함수] = useMutation(나의그래프큐엘셋팅)
+
+    const onClickSubmit = async () => {
+        const result = await 나의함수({
+            $: {        // $ = variables 변수 $ 대신 직접 variables 라고 써도 됨.
+                writer: writer,
+                title: title,
+                contents: contents
+            }
+        })
+        console.log(result)
+    }
+
+    const onChangeWriter = (event) => {
+        setWriter(event.target.value)
+    }
+
+    const onChangeTitle = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const onChangeContents = (event) => {
+        setContents(event.target.value)
+    }
+
+
+    return( 
+    <div>
+        작성자: <input type="text" onChange={onChangeWriter}/>
+        제목: <input type="text"  onChange={onChangeTitle}/>
+        내용: <input type="text"  onChange={onChangeContents}/>
+        <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+    </div>
+    )   
+    // 한줄로 할때는 괄호 () 필요 없음.
+
+}
