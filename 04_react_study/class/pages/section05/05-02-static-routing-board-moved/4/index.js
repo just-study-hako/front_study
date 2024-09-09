19955710707798c0bff1ae2faffd07775dc16e2e
mineki <mineki94@naver.com>
2024-09-09 16:28:13 +0900

    # 페이지 이동2
    하나의 페이지에서 여러 페이지로 이동하는 방식

---
+++ 04_react_study/class/pages/section05/05-02-static-routing-board-moved/4/index.js
@@ -0,0 +1,8 @@
+export default function StaticRoutingMovedPage() {
+    
+
+    return (
+        <div>4번 게시글 이동이 완료 되었습니다.</div>
+    )
+
+}
---
+++ 04_react_study/class/pages/section05/05-02-static-routing-board-moved/5/index.js
@@ -0,0 +1,8 @@
+export default function StaticRoutingMovedPage() {
+    
+
+    return (
+        <div>5번 게시글 이동이 완료 되었습니다.</div>
+    )
+
+}
