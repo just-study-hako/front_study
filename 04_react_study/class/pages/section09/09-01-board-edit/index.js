4534a0d05c78448437fcdb35a3f95e2e1d341ff0
mineki <mineki94@naver.com>
2024-09-16 18:04:15 +0900

    # 게시글 생성, 수정 페이지 만들기 - 비효율
    각각 페이지를 하드코딩으로 만드는 방법

---
+++ 04_react_study/class/pages/section09/09-01-board-edit/index.js
@@ -0,0 +1,10 @@
+export default function BoardEditPage() {
+    return (
+        <>
+            <h1>수정 페이지</h1>
+            제목 : <input type="text" /><br/>
+            내용 : <input type="text" /><br/>
+            <button>수정 하기</button>
+        </>  
+    )
+}
