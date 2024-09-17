22cdb36495aed8e2e86c3d8f798df48168b9d51e
mineki <mineki94@naver.com>
2024-09-17 21:54:12 +0900

    # graphQL 백엔드 새로만들기
    사용하던거 알수없는 에러가 발생해서 새로만듬

--- 04_react_study/back/demo/src/main/java/com/example/demo/graphql/repository/GraphqlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.graphql.repository;
-
-import com.example.demo.graphql.entity.Graphql;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GraphqlRepository extends JpaRepository<Graphql,Long> {
-}
